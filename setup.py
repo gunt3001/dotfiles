b21b4a390869c3c2f9bc3c4de418aead3ec56de5
Napat R. on Tae-PC WSL <napat.gunt3001@gmail.com>
2021-05-06 21:50:41 +0800

    Add check to prevent error on running on existing setup

--- setup.py
+++ setup.py
@@ -14,9 +14,10 @@ ONEDRIVE_DEST_MOUNT_DIR = os.path.join(HOMEDIR, "OneDrive")
 OHMYZSH_INSTALL_DIR = os.path.join(HOMEDIR, ".oh-my-zsh")
 
 # Install oh-my-zsh
-subprocess.run(["git", "clone", "https://github.com/ohmyzsh/ohmyzsh.git",
-                OHMYZSH_INSTALL_DIR], check=True)
-subprocess.run(["chsh", "--shell", "/usr/bin/zsh", "gun"])
+if not os.path.isdir(OHMYZSH_INSTALL_DIR):
+    subprocess.run(["git", "clone", "https://github.com/ohmyzsh/ohmyzsh.git",
+                    OHMYZSH_INSTALL_DIR], check=True)
+    subprocess.run(["chsh", "--shell", "/usr/bin/zsh", "gun"])
 
 # Install youtube-dl
 urllib.request.urlretrieve("https://yt-dl.org/downloads/latest/youtube-dl", "/usr/local/bin/youtube-dl")
@@ -25,10 +26,11 @@ os.chmod("/usr/local/bin/youtube-dl", 0o755)
 # Setup dotfiles
 # Set up dotfiles management system outlined in this tutorial
 # https://www.atlassian.com/git/tutorials/dotfiles
-subprocess.run(["git", "clone", "--bare", DOTFILES_REPO_URL,
-                DOTFILES_GIT_DIR], check=True)
-subprocess.run(["git", "--git-dir="+DOTFILES_GIT_DIR, "--work-tree="+HOMEDIR,
-                "checkout"], check=True)
+if not os.path.isdir(DOTFILES_GIT_DIR):
+    subprocess.run(["git", "clone", "--bare", DOTFILES_REPO_URL,
+                    DOTFILES_GIT_DIR], check=True)
+    subprocess.run(["git", "--git-dir="+DOTFILES_GIT_DIR, "--work-tree="+HOMEDIR,
+                    "checkout"], check=True)
 
 # Symlink to OneDrive mount to home directory, if exists
 if os.path.isdir(ONEDRIVE_SOURCE_MOUNT_DIR) and not os.path.isdir(ONEDRIVE_DEST_MOUNT_DIR):
