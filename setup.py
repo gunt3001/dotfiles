c650c8d26a6dd171c5a016691f9b791dce184ae9
Napat R <napat.gunt3001@gmail.com>
2022-11-21 22:52:49 +0700

    Overhaul of script with interactive installation and menu system. Also changed youtube-dl to yt-dlp

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ import subprocess
 import os
 import urllib.request
 
-# TODO: Installation flags and interactive mode
-
 HOMEDIR = "/home/gun"
 DOTFILES_REPO_URL = "https://github.com/gunt3001/dotfiles.git"
 DOTFILES_GIT_DIR = os.path.join(HOMEDIR, ".cfg")
@@ -13,30 +11,72 @@ ONEDRIVE_SOURCE_MOUNT_DIR = "/mnt/user/Home/OneDrive"
 ONEDRIVE_DEST_MOUNT_DIR = os.path.join(HOMEDIR, "OneDrive")
 OHMYZSH_INSTALL_DIR = os.path.join(HOMEDIR, ".oh-my-zsh")
 
-# Install oh-my-zsh
-if not os.path.isdir(OHMYZSH_INSTALL_DIR):
-    subprocess.run(["git", "clone", "https://github.com/ohmyzsh/ohmyzsh.git",
-                    OHMYZSH_INSTALL_DIR], check=True)
-    subprocess.run(["chsh", "--shell", "/usr/bin/zsh", "gun"])
+install_flags = {}
 
-# Install youtube-dl
-urllib.request.urlretrieve("https://yt-dl.org/downloads/latest/youtube-dl", "/usr/local/bin/youtube-dl")
-os.chmod("/usr/local/bin/youtube-dl", 0o755)
+def main():
+    print ("===== Starting personal setup script... =====")
 
-# Setup dotfiles
-# Set up dotfiles management system outlined in this tutorial
-# https://www.atlassian.com/git/tutorials/dotfiles
-if not os.path.isdir(DOTFILES_GIT_DIR):
-    subprocess.run(["git", "clone", "--bare", DOTFILES_REPO_URL,
-                    DOTFILES_GIT_DIR], check=True)
-    subprocess.run(["git", "--git-dir="+DOTFILES_GIT_DIR, "--work-tree="+HOMEDIR,
-                    "checkout"], check=True)
+    install_flags["oh-my-zsh"] = ask_option("Oh-My-Zsh")
+    install_flags["yt-dlp"] = ask_option("yt-dlp (install or update)")
+    install_flags["dotfiles"] = ask_option("Dotfiles")
+    install_flags["onedrive-symlink"] = ask_option("OneDrive symlink to home dir")
 
-# Symlink to OneDrive mount to home directory, if exists
-if os.path.isdir(ONEDRIVE_SOURCE_MOUNT_DIR) and not os.path.isdir(ONEDRIVE_DEST_MOUNT_DIR):
-    os.symlink(ONEDRIVE_SOURCE_MOUNT_DIR,
-               ONEDRIVE_DEST_MOUNT_DIR,
-               target_is_directory=True)
+    answer = input("Proceed? (y/n): ").lower()
+    if answer != "y":
+        return
 
-# [Finalize] Reset ownership in home dir
-subprocess.run(["chown", "-R", "gun:gun", HOMEDIR], check=True)
+    if install_flags["oh-my-zsh"]:
+        install_oh_my_zsh()
+    if install_flags["yt-dlp"]:
+        install_yt_dlp()
+    if install_flags["dotfiles"]:
+        setup_dotfiles()
+    if install_flags["onedrive-symlink"]:
+        setup_onedrive_symlink()
+
+    # [Finalize] Reset ownership in home dir
+    subprocess.run(["chown", "-R", "gun:gun", HOMEDIR], check=True)
+
+def install_oh_my_zsh():
+    if not os.path.isdir(OHMYZSH_INSTALL_DIR):
+        subprocess.run(["git", "clone", "https://github.com/ohmyzsh/ohmyzsh.git",
+                        OHMYZSH_INSTALL_DIR], check=True)
+        subprocess.run(["chsh", "--shell", "/usr/bin/zsh", "gun"])
+    else:
+        print("Oh-My-Zsh directory already exists. Skipping.")
+
+def install_yt_dlp():
+    urllib.request.urlretrieve("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp", "/usr/local/bin/yt-dlp")
+    os.chmod("/usr/local/bin/yt-dlp", 0o755)
+
+def setup_dotfiles():
+    # Setup dotfiles
+    # Set up dotfiles management system outlined in this tutorial
+    # https://www.atlassian.com/git/tutorials/dotfiles
+    if not os.path.isdir(DOTFILES_GIT_DIR):
+        subprocess.run(["git", "clone", "--bare", DOTFILES_REPO_URL,
+                        DOTFILES_GIT_DIR], check=True)
+        subprocess.run(["git", "--git-dir="+DOTFILES_GIT_DIR, "--work-tree="+HOMEDIR,
+                        "checkout"], check=True)
+    else:
+        print("Dotfiles git directory already exists in home. Skipping.")
+
+def setup_onedrive_symlink():
+    # Symlink to OneDrive mount to home directory, if exists
+    if os.path.isdir(ONEDRIVE_SOURCE_MOUNT_DIR) and not os.path.isdir(ONEDRIVE_DEST_MOUNT_DIR):
+        os.symlink(ONEDRIVE_SOURCE_MOUNT_DIR,
+                ONEDRIVE_DEST_MOUNT_DIR,
+                target_is_directory=True)
+    elif not os.path.isdir(ONEDRIVE_SOURCE_MOUNT_DIR):
+        print("OneDrive mount directory at " + ONEDRIVE_SOURCE_MOUNT_DIR + "does not exist. Skipping.")
+    elif os.path.isdir(ONEDRIVE_DEST_MOUNT_DIR):
+        print("OneDrive directory already exists on home. Skipping.")
+
+def ask_option(component):
+    valid_answers = ["y", "n"]
+    answer = ""
+    while answer not in valid_answers:
+        answer = input("Setup " + component + "? (y/n): ").lower()
+    return answer == "y"
+
+main()
