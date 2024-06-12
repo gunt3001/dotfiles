af3a7395d04d6d79830483235928f058f5c6beef
Gun on aoi-nas-admin <napat.gunt3001@gmail.com>
2024-06-13 02:26:42 +0700

    [nvim] Added GitHub Copilot plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -161,6 +161,9 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- GitHub Copilot
+  { 'github/copilot.vim' },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
