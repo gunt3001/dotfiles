da57f2b17505abf3fc32b55af67fdced63ac0371
Gun on aoi-nas-admin <napat.gunt3001@gmail.com>
2024-06-13 02:27:07 +0700

    [nvim] Enabled Python LSP and formatters

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -537,6 +537,9 @@ require('lazy').setup({
         },
 
         ltex = {},
+
+        -- Python LSP
+        pylsp = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -552,6 +555,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        -- Python formatters
+        'black',
+        'isort',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -598,7 +604,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
