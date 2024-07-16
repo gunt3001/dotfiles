d9b5675c4dcbcb89186c4678f54a1243c4ca2743
Napat Ratanakul <napat.gunt3001@gmail.com>
2024-07-16 15:57:23 +0700

    [nvim] disable <CR> and <Tab> autocompletion

--- .config/nvim/lua/kickstart/plugins/cmp.lua
+++ .config/nvim/lua/kickstart/plugins/cmp.lua
@@ -71,8 +71,8 @@ return {
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
           -- Also use Enter and Tab for auto-completion
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<Tab>'] = cmp.mapping.confirm { select = true },
+          -- ['<CR>'] = cmp.mapping.confirm { select = true },
+          -- ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
